f40b1db80512571dc8596dedcbe5215256708eb8
Tea Tran <tatran@seas.upenn.edu>
2020-02-20 10:52:34 -0500

    add

--- Fractal Terrain/Assets/Scripts/FractalTerrain.cs
+++ Fractal Terrain/Assets/Scripts/FractalTerrain.cs
@@ -2,25 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
 public class FractalTerrain : MonoBehaviour
 {
     // test
-    ArrayList triangles;
+    Mesh mesh;
+    ArrayList vertexLocList; // Vector3
+    ArrayList triangleList; // Trianlge
+    ArrayList triangleIdxList; // int
+ 
+    Vector3[] vertexArr;
+    int[] triangleIdxArr;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Test points
-        Vector3 p0_1 = new Vector3(-10, 0, 0);
-        Vector3 p0_2 = new Vector3(10, 0, 0);
-        Vector3 p0_3 = new Vector3(0, 0, 10);
-        triangles.add
+        mesh = new Mesh();
+        vertexLocList = new ArrayList();
+        triangleList = new ArrayList();
+        triangleIdxList = new ArrayList();
+        GetComponent<MeshFilter>().mesh = mesh;
+        CreateMesh();
+        UpdateMesh();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
     }
 
+    void CreateMesh()
+    {
+        // Test points
+        Vector3 v0 = new Vector3(-1, 0, 0);
+        Vector3 v1 = new Vector3(0, 0, 1);
+        Vector3 v2 = new Vector3(1, 0, 0);
+        int v0idx = 0;
+        int v1idx = 1;
+        int v2idx = 2;
+
+        vertexLocList.Add(v0);
+        vertexLocList.Add(v1);
+        vertexLocList.Add(v2);
+
+        triangleIdxList.Add(v0idx);
+        triangleIdxList.Add(v1idx);
+        triangleIdxList.Add(v2idx);
+
+        Triangle t = new Triangle(v0, v1, v2, v0idx, v1idx, v2idx);
+        ArrayList subTriangleList = t.Subdivide(ref vertexLocList);
+
+        for (int i = 0; i < subTriangleList.Count; i++)
+        {
+            ((Triangle)subTriangleList[i]).AddTriangleVertexIndices(ref triangleIdxList, ref triangleList);
+        }
+
+        ArrayList subTriangleList1 = ((Triangle)subTriangleList[subTriangleList.Count - 1]).Subdivide(ref vertexLocList);
+        for (int i = 0; i < subTriangleList1.Count; i++)
+        {
+            ((Triangle)subTriangleList1[i]).AddTriangleVertexIndices(ref triangleIdxList, ref triangleList);
+        }
+
+        TransferVerticesFromListToArray();
+        TransferIndicesFromListToArray();
+    }
+
+    void TransferVerticesFromListToArray()
+    {
+        vertexArr = new Vector3[vertexLocList.Count];
+        for (int i = 0; i < vertexLocList.Count; i++)
+        {
+            vertexArr[i] = (Vector3) vertexLocList[i];
+        }
+    }
+
+    void TransferIndicesFromListToArray()
+    { 
+        triangleIdxArr = new int[triangleIdxList.Count];
+        for (int i = 0; i < triangleIdxList.Count; i++)
+        {
+            triangleIdxArr[i] = (int) triangleIdxList[i];
+        }
+    }
+    
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertexArr;
+        mesh.triangles = triangleIdxArr;
+        mesh.RecalculateNormals();
+    }
 }
--- Fractal Terrain/Assets/Scripts/Triangle.cs
+++ Fractal Terrain/Assets/Scripts/Triangle.cs
@@ -2,81 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Triangle : MonoBehaviour
+public class Triangle //: MonoBehaviour
 {
     // WARNING: The order of the vertices is important because it affects the normal
     public Vector3 m_v0;
     public Vector3 m_v1;
     public Vector3 m_v2;
+    
+    // indices of the vertices within the mesh
+    public int m_v0idx;
+    public int m_v1idx;
+    public int m_v2idx;
+
     public Vector3 m_n; // normal of the triangle
 
     public Triangle() { }
 
-    // Constructor with vertices and normal as arguments
-    public Triangle(Vector3 v0, Vector3 v1, Vector3 v2)
+    // Constructor with vertices, their indices as arguments
+    // Calculate and set the normal of the trianlge and update the triangle index list and triangle list
+    public Triangle(Vector3 v0, Vector3 v1, Vector3 v2, int v0idx, int v1idx, int v2idx)
     {
         m_v0 = v0;
         m_v1 = v1;
         m_v2 = v2;
+        m_v0idx = v0idx;
+        m_v1idx = v1idx;
+        m_v2idx = v2idx;
         m_n = FindNormal();
     }
 
-    // Start is called before the first frame update
-    void Start() {}
-
-    // Update is called once per frame
-    void Update() {}
-
-    // Subdivide this triangle and return all the sub-triangles
-    public ArrayList Subdivide()
+    public void AddTriangleVertexIndices(ref ArrayList triangleIdxList, ref ArrayList triangleList)
     {
-        // Find the midpoints of edges of this triangle
-        Vector3 m0 = GetAdjustedMidpoint(m_v1, m_v2, m_n);
-        Vector3 m1 = GetAdjustedMidpoint(m_v2, m_v0, m_n);
-        Vector3 m2 = GetAdjustedMidpoint(m_v0, m_v1, m_n);
+        triangleIdxList.Add(m_v0idx);
+        triangleIdxList.Add(m_v1idx);
+        triangleIdxList.Add(m_v2idx);
+        triangleList.Add(this);
+    }
+
+    // Subdivide this triangle and update the input lists, return list of sub-triangles 
+    public ArrayList Subdivide(ref ArrayList vertexLocList)
+    {
+        Vector3 c = AdjustPointHeight(FindCentroid(), m_n);
+        int cidx = vertexLocList.Count;
+        vertexLocList.Add(c);
 
         // Create sub triangles and add to the list
-        Triangle t_m2v1m0 = new Triangle(m2, m_v1, m0);
-        Triangle t_v0m2m1 = new Triangle(m_v0, m2, m1);
-        Triangle t_m1m0v2 = new Triangle(m1, m0, m_v2);
-        Triangle t_m2m0m1 = new Triangle(m2, m0, m1);
+        Triangle t_v0v1c = new Triangle(m_v0, m_v1, c, m_v0idx, m_v1idx, cidx);
+        Triangle t_cv1v2 = new Triangle(c, m_v1, m_v2, cidx, m_v1idx, m_v2idx);
+        Triangle t_v0cv2 = new Triangle(m_v0, c, m_v2, m_v0idx, cidx, m_v2idx);
 
-        ArrayList subTriangles = new ArrayList();
-        subTriangles.Add(t_m2v1m0);
-        subTriangles.Add(t_v0m2m1);
-        subTriangles.Add(t_m1m0v2);
-        subTriangles.Add(t_m2m0m1);
+        ArrayList subTriangleList = new ArrayList();
+        subTriangleList.Add(t_v0v1c);
+        subTriangleList.Add(t_cv1v2);
+        subTriangleList.Add(t_v0cv2);
 
-        return subTriangles;
+        return subTriangleList;
+    }
+
+    Vector3 FindCentroid()
+    {
+        return 1f / 3f * (m_v0 + m_v1 + m_v2);
     }
 
     Vector3 FindNormal()
     {
-        Vector3 v10 = m_v0 - m_v1;
-        Vector3 v12 = m_v2 - m_v1;
-        return Vector3.Cross(v10, v12);
-    }
-
-    // Given two vertices, find the midpoint on the segment created by those and 
-    // adjust its height randomly based on the input direction to adjust the point along 
-    static Vector3 GetAdjustedMidpoint(Vector3 v0, Vector3 v1, Vector3 d)
-    {
-        Vector3 adjustedMidpoint = FindMidpoint(v0, v1);
-        adjustedMidpoint = AdjustPointHeight(adjustedMidpoint, d);
-        return adjustedMidpoint;
-    }
-
-    // Given two vertices, find the midpoint on the segment created by those
-    static Vector3 FindMidpoint(Vector3 v0, Vector3 v1)
-    {
-        return 0.5f * (v0 + v1);
+        Vector3 v01 = m_v1 - m_v0;
+        Vector3 v02 = m_v2 - m_v0;
+        return Vector3.Normalize(Vector3.Cross(v01, v02));
     }
 
     // Add some random values to the current height of this point along some direction
     static Vector3 AdjustPointHeight(Vector3 p, Vector3 d)
     {
         // Test to get random height
-        float rand = Random.Range(0f, 5f);
+        float rand = Random.Range(0f, 0.5f);
         return p + rand * d;
     }
 }
